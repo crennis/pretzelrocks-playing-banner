e95462d1613189c7f62b832ea4e39f96669a87ad
crennis <13422542+crennis@users.noreply.github.com>
2021-02-20 14:24:50 +0100

    Updated Banner
    Changed Text Size and added Transparency when paused.

--- pretzel-song-banner.py
+++ pretzel-song-banner.py
@@ -10,12 +10,13 @@ updateTime = 5 #Interval between Updates (Default 5 Seconds)
 includePretzelLogo = True #Inculed Pretzel Logo True/False (Default: True)
 
 #Custom Logo
-includeCustomLogo = False #Includes Custom Logo True/False (Default: False)
+includeCustomLogo = True #Includes Custom Logo True/False (Default: False)
 logoPath = "logo.png" #Path\to\logo.png
 logoPosition = (890,-10)
 
 #let it be
 old_track = ''
+returned = 0
 
 #Function for getting JSON Data
 def get_info():
@@ -27,11 +28,11 @@ def get_info():
 
 #Function to Genarate the Banner
 def gen_banner(track, artist):
-    #Fetch Data
-    cover = Image.open(PathToCover)
+    #get Cover
     #Creates the Background
     img = Image.new('RGB', (1000, 300), color = (23, 36, 45))
     #Pastes the Cover Art
+    cover = Image.open(PathToCover)
     img.paste(cover, (0,0))
     d = ImageDraw.Draw(img)
     #Adds Pretzel Logo if True
@@ -43,16 +44,26 @@ def gen_banner(track, artist):
         logo = Image.open(logoPath)
         img.paste(logo, logoPosition, logo.convert('RGBA'))
     #Writes down the Track and Artists
-    tfont = ImageFont.truetype("arial.ttf", 64)
-    afont = ImageFont.truetype("arial.ttf", 36)
+    tfont = ImageFont.truetype("arial.ttf", 80)
+    afont = ImageFont.truetype("arial.ttf", 40)
     d.text((310,50), track, font=tfont, fill=(255, 255, 240))
-    d.text((310,120), artist, font=afont, fill=(190, 190, 190))
+    d.text((310,140), artist, font=afont, fill=(190, 190, 190))
     #save
     img.save('banner.png')
 
-def update():
+def paused():
+    img = Image.new('RGBA', (1000, 300), color = (0, 0, 0, 0))
+    img.save('banner.png')
+
+def update(forced):
     global old_track
     track = data["track"]["title"]
+    if forced == 1:
+        old_track = track
+        get_info()
+        artist = data["track"]["artistsString"]
+        gen_banner(track, artist)
+        time.sleep(updateTime)
     if old_track == track:
         time.sleep(updateTime)
         return
@@ -63,6 +74,15 @@ def update():
         gen_banner(track, artist)
         time.sleep(updateTime)
 
+
 while True:
     get_info()
-    update()
+    if data["player"]["playing"] == False:
+        paused()
+        returned = 1
+        time.sleep(updateTime)
+    elif returned == 1:
+        update(1)
+        returned = 0
+    elif data["player"]["playing"] == True:
+        update(0)
